5f146b408705691de33ea738d3acff87aa2ebd73
PriyankaPuli <52269585+PriyankaPuli@users.noreply.github.com>
2020-12-23 14:08:16 +0530

    Update customer_test.cpp

--- Class and Objects/Customer/customer_test.cpp
+++ Class and Objects/Customer/customer_test.cpp
@@ -2,18 +2,18 @@
 #include<gtest/gtest.h>
 namespace{
     TEST(Customer,getBalance){
-        Customer c1("1","silpa","9846241285",400.0,Prepaid);
-        EXPECT_EQ(400.0, c1.getBalance());
+        Customer c1("1","Priya","8688354499",500.0,Prepaid);
+        EXPECT_EQ(500.0, c1.getBalance());
     }
     TEST(Customer,credit){
-        Customer c1("1","silpa","9846241285",400.0,Prepaid);
+        Customer c1("1","Priya","8688354499",500.0,Prepaid);
         c1.credit(300.0);
-        EXPECT_EQ(700.0, c1.getBalance());
+        EXPECT_EQ(800.0, c1.getBalance());
     }
     TEST(Customer,makecall){
-        Customer c1("1","silpa","9846241285",400.0,Prepaid);
-        c1.makeCall(3);
-        EXPECT_EQ(370.0, c1.getBalance());
+        Customer c1("1","Priya","8688354499",500.0,Prepaid);
+        c1.makeCall(4);
+        EXPECT_EQ(460.0, c1.getBalance());
     }
 
 }
